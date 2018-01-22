35eded248bf9ce713b84823f592aa79fd55b9779
Anuj Gupta <junagupta19@gmail.com>
2018-01-22 22:14:32 +0530

    Created MyApllication which will load as soon as the app loads

---
+++ app/src/main/java/in/androidmate/anujgupta/movify/MyApplication.java
@@ -0,0 +1,14 @@
+package in.androidmate.anujgupta.movify;
+
+import android.app.Application;
+
+/**
+ * Created by anujgupta on 22/01/18.
+ */
+
+public class MyApplication extends Application {
+
+
+
+
+}
